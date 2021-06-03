7038c1f607ee1cc25c416517be174672094850f8
marceloapuppio <marcelopuppio@gmail.com>
2021-06-03 16:23:33 -0300

    config vsc

---
+++ src/components/layout.js
@@ -0,0 +1,10 @@
+const Layout = ({children})=>{
+    return(
+            <>
+            <header>Encabezado</header>
+            {children}
+            <footer>Footer</footer>
+            </>
+
+    )
+}
